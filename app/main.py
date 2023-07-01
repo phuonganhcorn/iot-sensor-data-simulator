66b602f40362f1ac8ccfb791c6ce66530a3a01de
Anton Sarg <sarganton@gmail.com>
2023-07-01 15:33:39 +0200

    feat: Update IoTHubHelper class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-from nicegui import app, ui
+from nicegui import ui
+from dotenv import load_dotenv
 
+from utils.iot_hub_helper import IoTHubHelper
 from pages.containers_page import ContainersPage
 from pages.sensors_page import SensorsPage
 from pages.machines_page import MachinesPage
+
+load_dotenv()
+
+iot_hub_helper = IoTHubHelper()
 
 ContainersPage()
 
@@ -14,7 +20,7 @@ def sensors_page():
 
 @ui.page('/maschinen')
 def devices_page():
-    MachinesPage()
+    MachinesPage(iot_hub_helper)
 
 
 ui.run(title="IoT Telemetrie Simulator", host="127.0.0.1", port=8081)
