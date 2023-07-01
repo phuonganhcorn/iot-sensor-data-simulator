7e38b8942347ba5a7e9a12fd8a5eab345a862f3c
Anton Sarg <sarganton@gmail.com>
2023-07-01 11:06:55 +0200

    feat: Update navigation

--- app/components/navigation.py
+++ app/components/navigation.py
@@ -10,8 +10,8 @@ class Navigation():
         with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
             ui.label("IoT Telemetrie Simulator").classes(
                 'text-md font-semibold uppercase')
-            with ui.row():
+            with ui.row().classes('gap-12'):
                 ui.link('Container', '/').classes('text-white !no-underline')
                 ui.link('Sensoren', '/sensoren').classes('text-white !no-underline')
                 ui.link('Geräte', '/geraete').classes('text-white !no-underline')
-            ui.button(icon='menu').props('flat color=white')
+            ui.row()
