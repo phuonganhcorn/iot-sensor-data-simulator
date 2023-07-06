89faf0568c1679bf726b0b7556ce7db45422a245
Anton Sarg <sarganton@gmail.com>
2023-07-06 08:52:31 +0200

    feat: Add single anomalies

---
+++ app/constants/sensor_errors.py
@@ -0,0 +1,6 @@
+PROBABILITY_POS_ANOMALY = 'probability_pos_anomaly'
+PROBABILITY_NEG_ANOMALY = 'probability_neg_anomaly'
+POS_ANOMALY_UPPER_RANGE = 'pos_anomaly_upper_range'
+POS_ANOMALY_LOWER_RANGE = 'pos_anomaly_lower_range'
+NEG_ANOMALY_UPPER_RANGE = 'neg_anomaly_upper_range'
+NEG_ANOMALY_LOWER_RANGE = 'neg_anomaly_lower_range'
