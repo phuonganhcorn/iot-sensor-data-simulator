106374749e4ec15e9658a04572a07ee04a36a97d
Anton Sarg <sarganton@gmail.com>
2023-07-07 11:38:00 +0200

    feat: Add heading row to devices table

--- app/components/device_item.py
+++ app/components/device_item.py
@@ -8,8 +8,11 @@ class DeviceItem:
         self.visible = True
 
         with ui.row().bind_visibility(self, 'visible').classes('px-3 py-6 flex justify-between items-center w-full hover:bg-gray-50'):
-            with ui.row().classes('gap-12'):
-                ui.label(f'{device.name}')
+            with ui.row().classes('gap-6'):
+                ui.label(f'{device.id}').classes('w-[30px]')
+                ui.label(f'{device.name}').classes('w-[130px]')
+                ui.label(f'{device.container.name}').classes('w-[130px]')
+                ui.label(f'{len(device.sensors)}').classes('w-[60px]')
             with ui.row().classes('gap-2'):
                 ui.button(icon='delete', on_click=lambda d=device: delete_callback(d)).props(
                     'flat').classes('px-2 text-red')
