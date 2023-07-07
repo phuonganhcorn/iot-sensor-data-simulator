b44c5c63d870c716500a4ebe112c9ca3e4006f54
Anton Sarg <sarganton@gmail.com>
2023-07-07 16:30:35 +0200

    feat: Add container details dialog

--- app/components/container_card.py
+++ app/components/container_card.py
@@ -5,6 +5,7 @@ from components.logs_dialog import LogsDialog
 
 class ContainerCard():
     def __init__(self, wrapper, container, start_callback=None, stop_callback=None, delete_callback=None, live_view_callback=None):
+        self.wrapper = wrapper
         self.container = container
         self.card = None
         self.visible = True
@@ -25,10 +26,13 @@ class ContainerCard():
                 with ui.row().classes('pb-2 w-full justify-between items-center border-b border-gray-200'):
                     ui.label(container.name).classes('text-xl font-semibold')
                     with ui.row().classes('gap-0.5'):
-                        ui.button(icon='insert_chart_outlined', on_click=lambda: live_view_callback(container)).props('flat').classes('px-2 text-black')
+                        ui.button(icon='insert_chart_outlined', on_click=lambda: live_view_callback(
+                            container)).props('flat').classes('px-2 text-black')
                         with ui.button(icon='more_vert').props('flat').classes('px-2 text-black'):
                             with ui.menu().props(remove='no-parent-event'):
-                                ui.menu_item('Log anzeigen', lambda: self.show_logs_dialog(container)).classes(
+                                ui.menu_item('Details anzeigen', lambda: self.show_details_dialog()).classes(
+                                    'flex items-center')
+                                ui.menu_item('Logs anzeigen', lambda: self.show_logs_dialog(container)).classes(
                                     'flex items-center')
                                 ui.menu_item('Löschen', lambda w=wrapper, c=container, callback=delete_callback: self.show_delete_dialog(
                                     w, c, callback)).classes('text-red-500').classes('flex items-center')
@@ -69,6 +73,68 @@ class ContainerCard():
 
     def show_live_view_dialog(self, wrapper, container):
         LiveViewDialog(wrapper, container)
+
+    def show_details_dialog(self):
+        with self.wrapper:
+            with ui.dialog(value=True) as dialog, ui.card().classes("w-[696px] !max-w-none px-6 pb-6"):
+                self.dialog = dialog
+                with ui.row().classes("w-full justify-between items-center"):
+                    ui.label(
+                        f"Details - '{self.container.name}'").classes("text-xl font-semibold")
+                    ui.button(icon="close", on_click=self.dialog.close).props(
+                        "flat").classes("px-2 text-black")
+
+                with ui.row().classes("w-full flex justify-between"):
+                    with ui.column().classes("gap-4"):
+                        ui.label("Allgemein").classes(
+                            "text-lg font-semibold mt-2")
+                        with ui.row().classes("gap-10"):
+                            with ui.column().classes("gap-0"):
+                                ui.label("ID").classes("text-sm text-gray-500")
+                                ui.label(f"{self.container.id}").classes(
+                                    "text-md font-medium")
+                            with ui.column().classes("gap-0"):
+                                ui.label("Name").classes(
+                                    "text-sm text-gray-500")
+                                ui.label(f"{self.container.name}").classes(
+                                    "text-md font-medium")
+                            with ui.column().classes("gap-0"):
+                                ui.label("Standort").classes(
+                                    "text-sm text-gray-500")
+                                location = self.container.location
+                                ui.label(f"{location if location else 'k.A.'}").classes(
+                                    "text-md font-medium")
+                        with ui.row().classes("gap-10"):
+                            with ui.column().classes("gap-0"):
+                                ui.label("Beschreibung").classes(
+                                    "text-sm text-gray-500")
+                                description = self.container.description
+                                ui.label(f"{description if description else 'k.A.'}").classes(
+                                    "text-md font-medium")
+                    with ui.column().classes('pr-3 gap-1 items-end'):
+                        ui.label("Status").classes("text-sm text-gray-500")
+                        with ui.row().classes('gap-3 items-center'):
+                            ui.row().classes(
+                                f'h-4 w-4 rounded-full {"bg-green-500" if self.container.is_active else "bg-red-500"}')
+                            ui.label().bind_text_from(self.container, 'is_active',
+                                                      backward=lambda is_active: f'{"Aktiv" if is_active else "Inaktiv"}')
+
+                with ui.column().classes("gap-4"):
+                    ui.label("Struktur").classes("text-lg font-semibold mt-2")
+
+                    data = self.create_tree_data(self.container.devices)
+
+                    ui.tree(data, label_key='id')
+
+    def create_tree_data(self, devices):
+        tree = []
+        for device in devices:
+            device_node = {'id': device.name, 'children': []}
+            for sensor in device.sensors:
+                sensor_node = {'id': sensor.name}
+                device_node['children'].append(sensor_node)
+            tree.append(device_node)
+        return tree
 
     def show_logs_dialog(self, container):
         if not container.is_active:
