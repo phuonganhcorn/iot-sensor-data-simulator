8e131053771febd19717b5e22b24e1581ab82332
Anton Sarg <sarganton@gmail.com>
2023-07-09 23:27:30 +0200

    chore: Add SensorSelection component

--- app/components/live_view_dialog.py
+++ app/components/live_view_dialog.py
@@ -2,6 +2,7 @@ from nicegui import ui
 from model.device import Device
 from model.sensor import Sensor
 from constants.units import *
+from components.sensor_selection import SensorSelection
 
 
 class LiveViewDialog():
@@ -18,7 +19,8 @@ class LiveViewDialog():
                 self.dialog = dialog
                 with ui.row().classes("w-full justify-between items-center"):
                     self.title_label = ui.label().classes("text-lg font-semibold")
-                    ui.button(icon="close", on_click=self.dialog.close).props("flat").classes("px-2 text-black")
+                    ui.button(icon="close", on_click=self.dialog.close).props(
+                        "flat").classes("px-2 text-black")
                 with ui.row().classes():
                     self.selects_row = ui.row().classes("mb-5")
 
@@ -47,7 +49,7 @@ class LiveViewDialog():
                         "format": "{value}s"
                     }
                 },
-            }).classes("w-full h-64")      
+            }).classes("w-full h-64")
 
     def show(self, container):
         self.title_label.set_text(f"Live View: {container.name}")
@@ -58,37 +60,16 @@ class LiveViewDialog():
         with self.chart_wrapper:
             text = "Warten auf Sensordaten..." if container.is_active else "Container ist nicht aktiv"
             self.note_label = ui.label(text).classes("-translate-y-full")
-        
-
-        device_options = {
-            device.id: device.name for device in container.devices}
-        first_device = container.devices[0]
-        first_sensor = first_device.sensors[0] if len(
-            first_device.sensors) > 0 else None
-        sensor_options = {
-            sensor.id: sensor.name for sensor in container.devices[0].sensors}
 
         with self.selects_row:
-            self.device_select = ui.select(device_options, value=first_device.id,
-                                           label="Gerät", on_change=self.device_select_change_handler).classes("w-32")
-            first_sensor_value = first_sensor.id if first_sensor is not None else -1
-            self.sensor_select = ui.select(sensor_options, value=first_sensor_value,
-                                           label="Sensor", on_change=self.sensor_select_change_handler).classes("w-32")
-            
+            self.sensor_selection = SensorSelection(
+                container=container, sensor_select_callback=self.sensor_select_change_handler_test)
+
         self.dialog.open()
 
-    def device_select_change_handler(self):
-        sensor_name = self.update_sensor_select()
+    def sensor_select_change_handler_test(self, sensor):
         self.clear_chart()
-        self.update_series_name(sensor_name)
-
-    def sensor_select_change_handler(self):
-        self.clear_chart()
-        sensor_id = self.sensor_select.value
-        self.update_series_name(
-            self.sensor_select.options[sensor_id])
-        
-        sensor = Sensor.get_by_id(sensor_id)
+        self.update_series_name(sensor.name)
         self.update_y_axis(sensor)
 
     def clear_chart(self):
@@ -101,24 +82,9 @@ class LiveViewDialog():
 
     def update_y_axis(self, sensor):
         self.chart.options["yAxis"]["title"]["text"] = UNITS[sensor.unit]["name"]
-        self.chart.options["yAxis"]["labels"]["format"] = "{value} " + UNITS[sensor.unit]["unit_abbreviation"]
+        self.chart.options["yAxis"]["labels"]["format"] = "{value} " + \
+            UNITS[sensor.unit]["unit_abbreviation"]
         self.chart.update()
-
-    def update_sensor_select(self):
-        device_id = self.device_select.value
-        device = Device.get_by_id(device_id)
-
-        if len(device.sensors) > 0:
-            sensor_options = {
-                sensor.id: sensor.name for sensor in device.sensors}
-            self.sensor_select.options = sensor_options
-            first_value = list(sensor_options.keys())[0]
-            self.sensor_select.update()
-            self.sensor_select.value = first_value
-
-            self.update_y_axis(device.sensors[0])
-
-            return sensor_options[first_value]
 
     def append_value(self, sensor, value):
         # Return if the dialog is not open
@@ -126,14 +92,14 @@ class LiveViewDialog():
             return
 
         # Only append value if the correct device and the sensor is selected
-        if self.device_select.value != sensor.device_id or self.sensor_select.value != sensor.id:
+        if self.sensor_selection.device_select.value != sensor.device_id or self.sensor_selection.sensor_select.value != sensor.id:
             return
-        
+
         if self.chart is None:
             self.chart_wrapper.clear()
             self.setup_chart(sensor=sensor, init_data=[[0, value]])
             return
-        
+
         data = self.chart.options["series"][0]["data"]
         last_item = data[-1] if len(data) > 0 else None
         new_time = last_item[0] + 10 if last_item is not None else 0
